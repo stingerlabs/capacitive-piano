cae0ddd498832d0fee84d85129d6b904c8469f1c
pqmcgill <pqmcgill@gmail.com>
2018-10-24 16:19:55 -0400

    added websocket server

--- piano.py
+++ piano.py
@@ -7,6 +7,14 @@ from mingus.core import notes, chords
 from mingus.containers import * 
 from mingus.midi import fluidsynth
 import RPi.GPIO as GPIO #for GPIO switch
+from flask import Flask
+import socketio
+
+sio = socketio.Server(logger=True, async_mode=async_mode)
+app = Flask(__name__)
+app.wsgi_app = socketio.Middleware(sio, app.wsgi_app)
+app.config['SECRET_KEY'] = 'secret!'
+thread = None
 
 #fluidsynth.init('/usr/share/sounds/sf2/FluidR3_GM.sf2',"alsa")
 fluidsynth.init('/home/pi/Documents/Halloween Piano/soundFonts/Arachno SoundFont - Version 1.0.sf2',"alsa") #custom sound font sounds slightly better than FluidR3_GM.sf2, but file size is larger
@@ -169,35 +177,79 @@ print "Switches Ready."
 #when GPIO button on pin 20 is pressed (voltage rises from low to high), change mode
 GPIO.add_event_detect(20, GPIO.RISING, callback=changeMode) 
 
-#flash lights when ready
-for i in range(strip.numPixels()):
-    strip.setPixelColor(i, Color(255,165,0))
-strip.show()
-time.sleep(1)
-for i in range(strip.numPixels()):
-    strip.setPixelColor(i, Color(0,0,0))
-strip.show()
+current_pressed = [false] * 12;
 
-#listen for touch events on 12-key capacitive switch sensor
-while True:
-    try:
-        current_touched = cap.touched()
-        # Check each pin's last and current state to see if it was pressed or released.
-        for i in range(12):
-            # Each pin is represented by a bit in the touched value.  A value of 1
-            # means the pin is being touched, and 0 means it is not being touched.
-            pin_bit = 1 << i
-            # First check if transitioned from not touched to touched.
-            if current_touched & pin_bit and not last_touched & pin_bit:
-                #print '{0} touched!'.format(i)
-                activateKey(i)
-            # Next check if transitioned from touched to not touched.
-            if not current_touched & pin_bit and last_touched & pin_bit:
-                #print '{0} released!'.format(i)
-                deactivateKey(i)
-        # Update last state and wait a short period before repeating.
-        last_touched = current_touched
-        time.sleep(0.01)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-        exit()
+def handle_img_change(i):
+    current_pumpkins_pressed[i] = not current_pumpkins_pressed[i]
+    sio.emit('pumpkinsPressed', current_pressed)
+
+def backgroud_thread():
+    #listen for touch events on 12-key capacitive switch sensor
+    while True:
+        try:
+            current_touched = cap.touched()
+            # Check each pin's last and current state to see if it was pressed or released.
+            for i in range(12):
+                # Each pin is represented by a bit in the touched value.  A value of 1
+                # means the pin is being touched, and 0 means it is not being touched.
+                pin_bit = 1 << i
+                # First check if transitioned from not touched to touched.
+                if current_touched & pin_bit and not last_touched & pin_bit:
+                    #print '{0} touched!'.format(i)
+                    activateKey(i)
+                    handle_img_change(i)
+                # Next check if transitioned from touched to not touched.
+                if not current_touched & pin_bit and last_touched & pin_bit:
+                    #print '{0} released!'.format(i)
+                    deactivateKey(i)
+                    handle_img_change(i)
+            # Update last state and wait a short period before repeating.
+            last_touched = current_touched
+            time.sleep(0.01)
+        except KeyboardInterrupt:
+            GPIO.cleanup()
+            exit()
+
+
+# Configure websocket connection
+@sio.on('connect', namespace='/test')
+def test_connect(sid, environ):
+    sio.emit('my response', {'data': 'Connected', 'count': 0}, room=sid,
+             namespace='/test')
+
+
+@sio.on('disconnect', namespace='/test')
+def test_disconnect(sid):
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    thread = sio.start_background_task(background_thread)
+    if sio.async_mode == 'threading':
+        # deploy with Werkzeug
+        app.run(threaded=True)
+    elif sio.async_mode == 'eventlet':
+        # deploy with eventlet
+        import eventlet
+        import eventlet.wsgi
+        eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    elif sio.async_mode == 'gevent':
+        # deploy with gevent
+        from gevent import pywsgi
+        try:
+            from geventwebsocket.handler import WebSocketHandler
+            websocket = True
+        except ImportError:
+            websocket = False
+        if websocket:
+            pywsgi.WSGIServer(('', 5000), app,
+                              handler_class=WebSocketHandler).serve_forever()
+        else:
+            pywsgi.WSGIServer(('', 5000), app).serve_forever()
+    elif sio.async_mode == 'gevent_uwsgi':
+        print('Start the application through the uwsgi server. Example:')
+        print('uwsgi --http :5000 --gevent 1000 --http-websockets --master '
+              '--wsgi-file app.py --callable app')
+    else:
+        print('Unknown async_mode: ' + sio.async_mode)
+
